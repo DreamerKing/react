12a6a58b2aff388feb87b4db9f0326cab6e5ac7f
Dreamer King <dreamerking@live.cn>
2023-12-11 14:23:31 +0800

    redux

---
+++ src/ReduxApp.tsx
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementBy,
+} from "./features/counter/counterSlice.js";
+
+export default function ReduxApp() {
+  const counter = useSelector((state: any) => state.counter.value);
+  const dispactch = useDispatch();
+  return (
+    <div>
+      <p>{counter}</p>
+      <div>
+        <button onClick={() => dispactch(increment())}>Increment</button>
+        <button onClick={() => dispactch(decrement())}>Decrement</button>
+        <button onClick={() => dispactch(incrementBy(3))}>IncrementBy</button>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import LogRocket from "logrocket";
 // import App from "./App.tsx";
-import App from "./App";
+import App from "./ReduxApp.tsx";
+import store from "./store.ts";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
